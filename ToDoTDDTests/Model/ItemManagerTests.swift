fbb4dae3f3dd2f0dfada1ab0c8d99984b867b48b
linyuta <linyuta1230@gmail.com>
2019-04-14 18:19:20 +0800

    test adding one item

--- ToDoTDDTests/Model/ItemManagerTests.swift
+++ ToDoTDDTests/Model/ItemManagerTests.swift
@@ -11,9 +11,11 @@ import XCTest
 @testable import ToDoTDD
 
 class ItemManagerTests: XCTestCase {
+    
+    var sut: ItemManager!
 
     override func setUp() {
-        
+        sut = ItemManager()
     }
 
     override func tearDown() {
@@ -21,12 +23,15 @@ class ItemManagerTests: XCTestCase {
     }
     
     func testToDoCount_Initially_ShouldBeZero() {
-        let sut = ItemManager()
         XCTAssertEqual(sut.toDoCount, 0, "Initially toDo count should be 0")
     }
 
     func testDoneCount_Initially_ShouldBeZero() {
-        let sut = ItemManager()
         XCTAssertEqual(sut.doneCount, 0, "Initially done count should be 0")
+    }
+    
+    func testToDoCount_AfterAddingOneItem_IsOne() {
+        sut.addItem(item: ToDoItem(title: "Test title"))
+        XCTAssertEqual(sut.toDoCount, 1, "toDOCount should be 1")
     }
 }
