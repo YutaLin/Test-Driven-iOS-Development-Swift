461adc7077946498111388710e0ec3695ec21d72
linyuta <linyuta1230@gmail.com>
2019-04-14 17:10:57 +0800

    add ToDoItem and ToDoItemTests

---
+++ ToDoTDD/Model/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ToDoTDD
+//
+//  Created by Yuta Lin on 2019/4/14.
+//  Copyright © 2019 Yuta Lin. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    let title: String
+}
--- ToDoTDDTests/Model/ToDoItemTests.swift
+++ ToDoTDDTests/Model/ToDoItemTests.swift
@@ -1,5 +1,5 @@
 //
-//  ToDoTDDTests.swift
+//  ToDoItemTests.swift
 //  ToDoTDDTests
 //
 //  Created by Yuta Lin on 2019/4/14.
@@ -7,9 +7,10 @@
 //
 
 import XCTest
+
 @testable import ToDoTDD
 
-class ToDoTDDTests: XCTestCase {
+class ToDoItemTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,17 +19,11 @@ class ToDoTDDTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testInit_ShouldTakeTitle() {
+        let item = ToDoItem(title: "Test title")
+        XCTAssertNotNil(item, "item should not be nil")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
