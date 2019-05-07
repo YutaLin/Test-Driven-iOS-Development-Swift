ebe61a761d4ebd508fd8f1d1ec5b3f7fc080f983
linyuta <linyuta1230@gmail.com>
2019-05-07 11:12:35 +0800

    test init table view in ItemListViewController

---
+++ ToDoTDD/ItemListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ItemListViewController.swift
+//  ToDoTDD
+//
+//  Created by Yuta Lin on 2019/5/7.
+//  Copyright © 2019 Yuta Lin. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView()
+    }
+
+}
---
+++ ToDoTDDTests/ItemListViewControllerTests.swift
@@ -0,0 +1,30 @@
+//
+//  ItemListViewControllerTests.swift
+//  ToDoTDDTests
+//
+//  Created by Yuta Lin on 2019/5/7.
+//  Copyright © 2019 Yuta Lin. All rights reserved.
+//
+
+import XCTest
+
+@testable import ToDoTDD
+
+class ItemListViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_TableViewIsNotNilAfterViewDidLoad() {
+        let sut = ItemListViewController()
+        
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut.tableView)
+    }
+
+}
