d84165f49a4a462cbc0906dd23f70c4f08e5604a
linyuta <linyuta1230@gmail.com>
2019-05-07 13:47:12 +0800

    refine ItemListDataProviderTest, put duplicate initialization to setup

--- ToDoTDDTests/ItemListDataProviderTests.swift
+++ ToDoTDDTests/ItemListDataProviderTests.swift
@@ -11,9 +11,15 @@ import XCTest
 @testable import  ToDoTDD
 
 class ItemListDataProviderTests: XCTestCase {
+    
+    var sut: ItemListDataProvider!
+    var tableView: UITableView!
 
     override func setUp() {
-        
+        sut = ItemListDataProvider()
+        sut.itemManager = ItemManager()
+        tableView = UITableView()
+        tableView.dataSource = sut
     }
 
     override func tearDown() {
@@ -21,20 +27,11 @@ class ItemListDataProviderTests: XCTestCase {
     }
 
     func test_NumberOfSections_IsTwo() {
-        let sut = ItemListDataProvider()
-        let tableView = UITableView()
-        tableView.dataSource = sut
-        
         let numberOfSections = tableView.numberOfSections
         XCTAssertEqual(numberOfSections, 2)
     }
     
     func test_NumberOfRows_Section1_isToDoCount() {
-        let sut = ItemListDataProvider()
-        sut.itemManager = ItemManager()
-        let tableView = UITableView()
-        tableView.dataSource = sut
-        
         sut.itemManager?.addItem(item: ToDoItem(title: "Foo"))
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
         sut.itemManager?.addItem(item: ToDoItem(title: "Bar"))
