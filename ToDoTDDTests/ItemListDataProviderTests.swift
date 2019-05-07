1185ad777747c80d158f567c6a093ff2afd3ebb6
linyuta <linyuta1230@gmail.com>
2019-05-07 18:51:34 +0800

    test delete button title

--- ToDoTDDTests/ItemListDataProviderTests.swift
+++ ToDoTDDTests/ItemListDataProviderTests.swift
@@ -25,6 +25,7 @@ class ItemListDataProviderTests: XCTestCase {
         
         tableView = controller.tableView
         tableView.dataSource = sut
+        tableView.delegate = sut
     }
 
     override func tearDown() {
@@ -97,6 +98,19 @@ class ItemListDataProviderTests: XCTestCase {
         let cell = mockTableView.cellForRow(at: IndexPath(row: 0, section: 1)) as! MockItemCell
         
         XCTAssertEqual(cell.catchedItem, second)
+    }
+    
+    func test_DeleteButton_InFirstSection_ShowsTitleCheck() {
+        let deleteButtonTitle = tableView.delegate?.tableView?(tableView, titleForDeleteConfirmationButtonForRowAt: IndexPath(row: 0, section: 0))
+        
+        XCTAssertEqual(deleteButtonTitle, "Check")
+    }
+    
+    func test_DeleteButton_InSecondSection_ShowsTitleUnCheck() {
+        let deleteButtonTitle = tableView.delegate?.tableView?(tableView, titleForDeleteConfirmationButtonForRowAt: IndexPath(row: 0, section: 1))
+        
+        XCTAssertEqual(deleteButtonTitle, "UnCheck")
+        
     }
 }
 
