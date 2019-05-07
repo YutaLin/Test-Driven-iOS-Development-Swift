cfcd63dd14ffbcaa00ebe8fe8c8b82fdf9649f17
linyuta <linyuta1230@gmail.com>
2019-05-07 16:02:31 +0800

    mock TableView to test if cell got dequeued

--- ToDoTDDTests/ItemListDataProviderTests.swift
+++ ToDoTDDTests/ItemListDataProviderTests.swift
@@ -53,9 +53,36 @@ class ItemListDataProviderTests: XCTestCase {
     
     func test_CellForRow_ReturnsItemCell() {
         sut.itemManager?.addItem(item: ToDoItem(title: "Foo"))
+        tableView.register(ItemCell.self, forCellReuseIdentifier: "ItemCell")
         tableView.reloadData()
         
         let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
         XCTAssertTrue(cell is ItemCell)
+    }
+    
+    func test_CellForRow_DequeusCellFromTableView() {
+        let mockTableView = MockTableView()
+        mockTableView.dataSource = sut
+        mockTableView.register(ItemCell.self, forCellReuseIdentifier: "ItemCell")
+
+        sut.itemManager?.addItem(item: ToDoItem(title: "Foo"))
+        mockTableView.reloadData()
+        
+        _ = mockTableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        
+        XCTAssertTrue(mockTableView.cellGotDequeued)
+    }
+}
+
+extension ItemListDataProviderTests {
+    
+    class MockTableView: UITableView {
+        var cellGotDequeued = false
+        
+        override func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+            cellGotDequeued = true
+            
+            return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        }
     }
 }
