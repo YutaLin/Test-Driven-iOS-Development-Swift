21deb32cb7fd59fcd4c8bf7905c84a57f2be0b51
linyuta <linyuta1230@gmail.com>
2019-05-10 23:03:44 +0800

    add DetailViewController

---
+++ ToDoTDD/Controller/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  ToDoTDD
+//
+//  Created by Yuta Lin on 2019/5/10.
+//  Copyright © 2019 Yuta Lin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+}
