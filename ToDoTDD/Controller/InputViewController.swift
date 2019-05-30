1c76160976415a8cfe0e1836276322a2e14c97fe
linyuta <linyuta1230@gmail.com>
2019-05-30 12:29:20 +0800

    add ui element on InputViewController

--- ToDoTDD/Controller/InputViewController.swift
+++ ToDoTDD/Controller/InputViewController.swift
@@ -12,6 +12,12 @@ class InputViewController: UIViewController {
     
     
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
