1282e8681b72e5c3a7d66afecafbc6e0cd42a475
linyuta <linyuta1230@gmail.com>
2019-05-30 12:02:30 +0800

    add InputViewController

---
+++ ToDoTDD/Controller/InputViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InputViewController.swift
+//  ToDoTDD
+//
+//  Created by Yuta Lin on 2019/5/30.
+//  Copyright © 2019 Yuta Lin. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
